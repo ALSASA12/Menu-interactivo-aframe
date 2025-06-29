bed0b166d4b7ee4e9329c921049e67a07d65024e
ALSASA12 <a.sanchezsa.2018@alumnos.urjc.es>
2025-06-29 03:01:32 +0200

    cambios por bug

--- Demo_Habitacion/js/pressable.js
+++ Demo_Habitacion/js/pressable.js
@@ -1,78 +1,54 @@
-AFRAME.registerSystem('pressable-manager', {
-  init: function () {
-    this.activePress = null;
-  },
-
-  setActivePress: function (el) {
-    this.activePress = el;
-  },
-
-  clearActivePress: function () {
-    this.activePress = null;
-  },
-
-  isPressActive: function () {
-    return this.activePress !== null;
-  }
-});
-
+/* global AFRAME, THREE */
 AFRAME.registerComponent('pressable', {
   schema: {
-    pressDistance: { default: 0.05 },
-    delay: { default: 1000 } // Puedes configurarlo por entidad
+    pressDistance: { default: 0.05 }
   },
 
   init: function () {
     this.worldPosition = new THREE.Vector3();
     this.handEls = document.querySelectorAll('[hand-tracking-controls]');
     this.pressed = false;
-    this.pressStartTime = null;
-    this.system = this.el.sceneEl.systems['pressable-manager'];
   },
 
   tick: function () {
-    const now = Date.now();
-
-    if (this.system.isPressActive() && this.system.activePress !== this.el) {
-      this.reset();
+    if (AFRAME.scenes[0].pressedObject && AFRAME.scenes[0].pressedObject !== this.el) {
+      // Otro objeto ya está siendo presionado, no hacer nada.
       return;
     }
 
-    for (let i = 0; i < this.handEls.length; i++) {
-      const handEl = this.handEls[i];
-      const distance = this.calculateFingerDistance(handEl.components['hand-tracking-controls'].indexTipPosition);
-
+    var handEls = this.handEls;
+    var handEl;
+    var distance;
+    for (var i = 0; i < handEls.length; i++) {
+      handEl = handEls[i];
+      distance = this.calculateFingerDistance(handEl.components['hand-tracking-controls'].indexTipPosition);
       if (distance < this.data.pressDistance) {
-        if (this.pressStartTime === null) {
-          this.pressStartTime = now;
-        }
-
-        if (!this.pressed && now - this.pressStartTime >= this.data.delay) {
+        if (!this.pressed) {
           this.el.emit('pressedstarted');
-          this.system.setActivePress(this.el);
-          this.pressed = true;
+          AFRAME.scenes[0].pressedObject = this.el;
         }
+        this.pressed = true;
         return;
       }
     }
 
     if (this.pressed) {
       this.el.emit('pressedended');
-      this.system.clearActivePress();
+      if (AFRAME.scenes[0].pressedObject === this.el) {
+        AFRAME.scenes[0].pressedObject = null;
+      }
     }
-    this.reset();
-  },
-
-  reset: function () {
     this.pressed = false;
-    this.pressStartTime = null;
   },
 
   calculateFingerDistance: function (fingerPosition) {
-    const worldPosition = this.worldPosition;
-    worldPosition.copy(this.el.object3D.position);
-    this.el.object3D.parent.updateMatrixWorld();
-    this.el.object3D.parent.localToWorld(worldPosition);
+    var el = this.el;
+    var worldPosition = this.worldPosition;
+
+    worldPosition.copy(el.object3D.position);
+    el.object3D.parent.updateMatrixWorld();
+    el.object3D.parent.localToWorld(worldPosition);
+
     return worldPosition.distanceTo(fingerPosition);
   }
 });
